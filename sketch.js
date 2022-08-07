b27896a4993c856ba40ca36524f4b178794a62fa
Tomat05 <wetherill.t@outlook.com>
2022-08-07 22:02:13 +0100

    SPHERE

--- sketch.js
+++ sketch.js
@@ -1,47 +1,50 @@
-let cols, rows;
-let w = 2000, h = 1000;
-let scale = 20;
+let res = 40;
+let r = 200;
+let globe = [];
 
-let flying = 0;
+let rotation = 0;
 
-let terrain = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+	createCanvas(windowWidth, windowHeight, WEBGL);
 
-  cols = w / scale;
-  rows = h / scale;
+	for (let i = 0; i <= res; i++) {
+		let lat = map(i, 0, res, -HALF_PI, HALF_PI);
+		globe[i] = [];
+		for (let j = 0; j <= res; j++) {
+			let lon = map(j, 0, res, -PI, PI);
+
+			let x = r * sin(lon) * cos(lat);
+			let y = r * sin(lon) * sin(lat);
+			let z = r * cos(lon);
+
+			globe[i].push(createVector(x + map(noise(x), 0, 1, -10, 10), y + map(noise(y), 0, 1, -10, 10), z + map(noise(z), 0, 1, -5, 5)));
+		}
+	}
 }
 
+
+
 function draw() {
-  angleMode(DEGREES);
-  background(50);
+	background(0);
+	directionalLight(255, 255, 255, -1, -1, 0);
 
-  flying -= 0.01;
+	//stroke(0);
+	noStroke();
+	rotateX(80);
+	//rotateZ(rotation);
+	for (let i = 0; i < res; i++) {
+		beginShape(TRIANGLE_STRIP);
+		for (let j = 0; j <= res; j++) {
+			let v1 = globe[i][j];
+			let v2 = globe[i+1][j];
 
-  let yOffset = flying;
-  for (let y = 0; y < rows; y++) {
-    let xOffset = 0;
-    terrain[y] = [];
-    for (let x = 0; x < cols; x++) {
-      terrain[y].push(map(noise(xOffset, yOffset), 0, 1, -100, 100));
-      xOffset += 0.1;
-    }
-    yOffset += 0.1;
-  }
+			ambientMaterial(0, 175, 255);
+			vertex(v1.x, v1.y, v1.z);
+			vertex(v2.x, v2.y, v2.z);
+		}
+		endShape();
+	}
 
-  push();
-  fill(0, 0, 255);
-  stroke(255);
-  rotateX(60);
-  translate(-w / 2, -h / 2)
-  for (let y = 0; y < rows-1; y++) {
-    beginShape(TRIANGLE_STRIP);
-    for (let x = 0; x < cols; x++) {
-      vertex(x * scale, y * scale, terrain[y][x]);
-      vertex(x * scale, (y + 1) * scale, terrain[y+1][x]);
-    }
-    endShape();
-  }
-  pop();
+	rotation += 0.005;
 }
