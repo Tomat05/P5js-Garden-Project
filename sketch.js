b314e22235007ec5f6082554bde8fb0cb8ea5d37
Tomat05 <wetherill.t@outlook.com>
2022-08-07 14:42:08 +0100

    E

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let cols, rows;
+let scale = 20;
+
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 }
 
 function draw() {
   background(50);
+
+  push();
+  for (let y = 0; y < rows; y++) {
+    beginShape(TRIANGLE_STRIP);
+    for (let x = 0; x < cols; x++) {
+      vertex(x * scale, y * scale);
+    }
+    endShape(CLOSE);
+  }
+  pop();
 }
