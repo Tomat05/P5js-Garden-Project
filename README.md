1e3942d309598f0506a1f26a56a0f481880c1730
Tomat05 <wetherill.t@outlook.com>
2022-08-11 19:42:00 +0100

    I Hate P5js, an essay by Thomas Wetherill

--- README.md
+++ README.md
@@ -3,13 +3,14 @@
 ## Summary
 
 - I originally tried doing this with a cube-sphere (essentially 6 faces, arrange them as a cube, then force each vertex to be equidistant from the centre)
-as this results in a much more even spread of detail than a UV sphere, however, I couldn't find a way to force the points in.
+as this results in a much more even spread of detail than a UV sphere.
 - However, at this point I was on about iteration 8 having tried:
     - An early version of this planet thing that was extremely broken
     - 3d procedural terrain (I couldn't decide how to make it garden-themed)
     - Multiple versions of a "design your own garden" thing using a grid of tiles which then turned into...
     - Longer than I would like to admit trying to create a procedural garden generator using a wavefuntion collapse algorithm (however I currently lack the skills to follow through fully with)
-    - Attempting the aforementioned cube-sphere thing before finding a coding train video on a UV sphere and deciding it was good enough for my purposes
+    - Attempt two of the aforementioned cube-sphere
+- Eventually I found a coding train video on a UV sphere and decided it was good enough for my purposes.
 
 ## Thoughts
 
--- sketch.js
+++ sketch.js
@@ -21,17 +21,22 @@ let starSize = [];
 let starColours = [];
 
 // Trees
-let trees;
-let numtrees = 30;
+let trees = [];
+let numtrees = 100;
+let tree1;
+let tree2;
 
-let camera;
 let title;
-let rotation = 0;
-
+let rotationX = 0;
+let rotationY = 0;
+let mouseXOld;
+let mouseYOld;
 
 
 function preload() {
     title = loadImage("P5Title.png"); // Using a texture as the in-built text requires 'createGraphic()' in WebGL mode which drastically decreases performance
+
+    tree1 = loadModel("tree1.obj");
 }
 
 
@@ -42,12 +47,14 @@ function setup() {
 
     document.oncontextmenu = function() { return false; } // No menu appears on right click
 
-    camera = createCamera();
+    mouseXOld = mouseX; // Set initial mouseXOld and mouseYOld
+    mouseYOld = mouseY;
 
+    CreateStars(); // Generate everything
     Generate();
 
     // Button to save picture of canvas
-    let saveButton = createButton("save");
+    let saveButton = createButton("Save");
     saveButton.style("width: 75px; height: 30px");
     saveButton.position((width / 2) - (37.5), 120);
     saveButton.mousePressed(Save);
@@ -59,16 +66,18 @@ function draw() {
 	background(0);
     angleMode(DEGREES); // WHY. IS. THE. DEFAULT. RADIANS. ALSKJDHSLDKJF >:/
 
-    // Title
+    // TITLE
     push();
-    translate(-288, -height / 2 + 20);
+    translate(-218.5, -height / 2 + 20);
     texture(title);
-    rect(0, 0, 576, 324);
+    rect(0, 0, 437, 66.5);
     pop();
 
-	directionalLight(255, 255, 255, -0, 0, -1);
+    // LIGHTS
+	directionalLight(255, 255, 255, 0, 0, -1);
+    //ambientLight(255);
 
-    // Draw stars
+    // STARS
 	push();
 	translate(0, 0, -300);
 	for (let i = 0; i < numStars; i++) {
@@ -78,12 +87,13 @@ function draw() {
 	}
 	pop();
 
-	//ambientLight(255);
 
+    // PLANET
 	push();
 	noStroke();
 	//rotateX(90);
-	//rotateZ(rotation);
+    //rotateZ(rotationX);
+    //rotateX(rotationY);
 
     // Draw Planet
 	for (let i = 0; i < res1; i++) {
@@ -93,62 +103,50 @@ function draw() {
 			let v1 = globe[i][j];
 			let v2 = globe[i+1][j];
 
-            fill(v1.x, v1.y, v1.z);
 			//ambientMaterial(terrainColours[i][j]);
+            fill(255);
 			normal(v1.x, v1.y, v1.z);
 			vertex(v1.x, v1.y, v1.z);
 
             //ambientMaterial(terrainColours[i+1][j]);
-            fill(v2.x, v2.y, v2.z);
 			normal(v2.x, v2.y, v2.z);
 			vertex(v2.x, v2.y, v2.z);
+
+            // DETAILS
+            // if (trees[i][j] === true) {
+            //     push();
+            //     emissiveMaterial(i * 2);
+            //     translate(v1.x - 10, v1.y + 20, v1.z + 20);
+            //     rotateZ(map(i / 2, 0, res1 / 2, 0, 180));
+            //     // rotateY(90 + map(j, 0, res1, 360, 0));
+            //     cone(20, 50);
+            //     // scale(15);
+            //     // model(tree1);
+            //     pop()
+            // };
 		}
 		endShape();
 		pop();
 	}
 
+    // ATMOSPHERE
     // push();
     // noStroke();
-    // for (let i = 0; i < res2; i++) {
-    //     push();
-    //     beginShape(TRIANGLE_STRIP);
-    //     for (let j = 0; j <= res2; j++) {
-    //         let v1 = atmos[i][j];
-    //         let v2 = atmos[i+1][j];
-
-    //         fill(baseAtmos);
-    //         normal(v1.x, v1.y, v1.z);
-    //         vertex(v1.x, v1.y, v1.z);
-
-    //         normal(v2.x, v2.y, v2.z);
-    //         vertex(v2.x, v2.y, v2.z);
-    //     }
-    //     endShape();
-    //     pop();
-    // }
+    // fill(baseAtmos);
+    // sphere(r2);
     pop();
 
-    // // Draw details
-    // push();
-    // noStroke();
-    // for (let i = 0; i < numtrees; i++) {
-    //     emissiveMaterial(255);
-    //     translate(trees[i]);
-    //     box(10, 10, 10);
-    // }
-    // pop();
-
-	rotation += 0.5;
-    //print(camera.eyeX, camera.eyeY, camera.eyeZ);
+    // Set old positions to new positions ready for next frame
+	mouseXOld = mouseX;
+    mouseYOld = mouseY;
 }
 
 
 // General generation function
 function Generate() {
-    CreateStars();
+    CreateDetails();
     CreatePlanet();
     CreateAtmos();
-    CreateDetails();
 
     generations++;
 }
@@ -157,22 +155,22 @@ function Generate() {
 
 // Creates a planet
 function CreatePlanet() {
-    // Terrain and atmosphere colours
+    // Terrain colours
 	baseTerrain = color(random(50, 255), random(50, 255), random(50, 255));
     let colourShift = random(-100, -20);
     terrainDetail = color(red(baseTerrain) + colourShift, green(baseTerrain) + colourShift, blue(baseTerrain) + colourShift);
 
     // Create array of vertices for terrain
 	for (let i = 0; i <= res1; i++) {
-        let lon = map(i, 0, res1, -180, 180);
+        let lon = map(i, 0, res1, -90, 90);
         terrainColours[i] = []; // Create 2d array + clear array on multiple generations
 		globe[i] = [];
 		for (let j = 0; j <= res1; j++) {
-			let lat = map(j, 0, res1, -90, 90);
+			let lat = map(j, 0, res1, -180, 180);
 
-			let x = r1 * sin(lon) * cos(lat);
-			let y = r1 * sin(lon) * sin(lat);
-			let z = r1 * cos(lon);
+			let x = r1 * sin(lat) * cos(lon);
+			let y = r1 * sin(lat) * sin(lon);
+			let z = r1 * cos(lat);
 
             // These are needed otherwise the noise stays the same until reload rather than changing every new planet
             let nX = x * generations;
@@ -180,7 +178,6 @@ function CreatePlanet() {
             let nZ = z * generations;
 
             globe[i].push(createVector(x + map(noise(nX, nY, nZ), 0, 1, -10, 10), y + map(noise(nY, nX, nZ), 0, 1, -10, 10), z + map(noise(nZ, nX, nY), 0, 1, -10, 10)));
-            //globe[i].push(createVector(x, y, z));
 
             // Choose between base and detail colour for each vertex
             let whichColour = round(random());
@@ -199,32 +196,17 @@ function CreatePlanet() {
 
 
 
-// Creates an atmosphere
+// Atmospheric colour and radius
 function CreateAtmos(){
     baseAtmos = color(random(100, 255), random(100, 255), random(100, 255), random(75, 125));
-    r2 = random(210, 220); // atmosphere radius
-
-    // Create array of vertices for atmosphere
-    for (let i = 0; i <= res2; i++) {
-        let lon = map(i, 0, res2, -180, 180);
-		atmos[i] = [];
-		for (let j = 0; j <= res2; j++) {
-			let lat = map(j, 0, res2, -90, 90);
-
-			let x = r2 * sin(lon) * cos(lat);
-			let y = r2 * sin(lon) * sin(lat);
-			let z = r2 * cos(lon);
-
-			atmos[i].push(createVector(x, y, z));
-		}
-	}
+    r2 = random(250, 300); // atmosphere radius
 }
 
 
 
 // Creates background stars
 function CreateStars() {
-    // Create arrays attributes & positions for stars
+    // Create arrays of attributes & positions for stars
 	for (let i = 0; i < numStars; i++) {
 		starsX.push(random(-1920, 1920)); // Using an array of vectors didn't work so using separate arrays for x and y 
 		starsY.push(random(-1080, 1080));
@@ -236,13 +218,22 @@ function CreateStars() {
 
 
 function CreateDetails() {
-    trees = []; // Clear trees list
+    trees = []; // Clear trees array on regenerate
 
-    // Create array of vertices for trees
+    // Populate trees array
+    for (let i = 0; i <= res1; i++) {
+        trees[i] = [];
+        for (let j = 0; j < res1; j++) {
+            trees[i].push(false);
+        }
+    }
+
+    // Replace some values with true (signifying a tree will be placed there)
     for (let i = 0; i < numtrees; i++) {
-        let mainList = floor(random(globe.length)); // ---------->
-        let subList = floor(random(globe[mainList].length));// --> because it's a 2d array
-        trees.push(globe[mainList][subList]);
+        let randMain = floor(random(res1));
+        let randSub = floor(random(res1));
+        
+        trees[randMain][randSub] = true;
     }
 }
 
@@ -261,5 +252,15 @@ function Save() {
 function keyPressed() {
     if (keyCode === ENTER) {
         Generate();
+    }
+}
+
+
+
+// Rotation stuff on mouse drag
+function mouseDragged() {
+    if (mouseButton === LEFT) {
+        rotationX -= (mouseX - mouseXOld) / 4;
+        rotationY -= (mouseY - mouseYOld) / 4;
     }
 }
