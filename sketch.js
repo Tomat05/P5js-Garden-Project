96c36563db48b29f4791c23fef702859ac231720
Tomat05 <wetherill.t@outlook.com>
2022-08-11 10:46:29 +0100

    Update README.md

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ let globe = [];
 let baseTerrain;
 let terrainDetail;
 let terrainColours = [];
-let clicks = 1;
+let generations = 1;
 
 // Atmosphere
 let res2 = 70;
@@ -33,86 +33,13 @@ let rotation = 0;
 
 function setup() {
     angleMode(DEGREES);
-	let canv = createCanvas(windowWidth, windowHeight, WEBGL);
+	createCanvas(windowWidth, windowHeight, WEBGL);
 
     document.oncontextmenu = function() { return false; } // No menu appears on right click
-    canv.mousePressed(setup);
 
-    trees = []; // Clear trees list
+    Generate();
 
-    // Terrain and atmosphere colours
-	baseTerrain = color(random(50, 255), random(50, 255), random(50, 255));
-    let colourShift = random(-100, -20);
-    terrainDetail = color(red(baseTerrain) + colourShift, green(baseTerrain) + colourShift, blue(baseTerrain) + colourShift);
-
-    baseAtmos = color(random(100, 255), random(100, 255), random(100, 255), random(75, 125));
-    r2 = random(210, 220); // atmosphere resolution
-
-    // Create list of vertices for terrain
-	for (let i = 0; i <= res1; i++) {
-        let lon = map(i, 0, res1, -180, 180);
-        terrainColours[i] = [];
-		globe[i] = [];
-		for (let j = 0; j <= res1; j++) {
-			let lat = map(j, 0, res1, -90, 90);
-
-			let x = r1 * sin(lon) * cos(lat);
-			let y = r1 * sin(lon) * sin(lat);
-			let z = r1 * cos(lon);
-
-            // These are needed otherwise planet terrain stays the same until reload rather than changing every click
-            let cX = x * clicks;
-            let cY = y * clicks;
-            let cZ = z * clicks;
-
-            globe[i].push(createVector(x + map(noise(cX, cY, cZ), 0, 1, -10, 10), y + map(noise(cY, cX, cZ), 0, 1, -10, 10), z + map(noise(cZ, cX, cY), 0, 1, -10, 10)));
-
-            let whichColour = round(random());
-            switch (whichColour) {
-                case 1:
-                    terrainColours[i].push(baseTerrain);
-                    break;
-                
-                default:
-                    terrainColours[i].push(terrainDetail);
-                    break;
-            }
-		}
-	}
-
-
-    // Create list of vertices for atmosphere
-    for (let i = 0; i <= res2; i++) {
-        let lon = map(i, 0, res2, -180, 180);
-		atmos[i] = [];
-		for (let j = 0; j <= res2; j++) {
-			let lat = map(j, 0, res2, -90, 90);
-
-			let x = r2 * sin(lon) * cos(lat);
-			let y = r2 * sin(lon) * sin(lat);
-			let z = r2 * cos(lon);
-
-			atmos[i].push(createVector(x, y, z));
-		}
-	}
-
-    // Create lists attributes & positions for stars
-	for (let i = 0; i < numStars; i++) {
-		starsX.push(random(-1920, 1920));
-		starsY.push(random(-1080, 1080));
-		starSize.push(random(1, 2));
-		starColours.push(color(random(200, 255), random(200, 255), random(200, 255)));
-	}
-
-    // Create list of vertices for trees
-    for (let i = 0; i < numtrees; i++) {
-        let mainList = floor(random(globe.length));
-        let subList = floor(random(globe[mainList].length));
-        trees.push(globe[mainList][subList]);
-    }
-
-    clicks++;
-
+    // Button to save picture of canvas
     let saveButton = createButton("save");
     saveButton.style("width: 75px; height: 30px");
     saveButton.position((width / 2) - (37.5), 25);
@@ -185,8 +112,8 @@ function draw() {
 
     push();
     for (let i = 0; i < numtrees; i++) {
-        translate(trees[i]);
         emissiveMaterial(255);
+        translate(trees[i]);
         box(10, 10, 10);
     }
     pop();
@@ -197,8 +124,121 @@ function draw() {
 
 
 
+function Generate() {
+    CreateStars();
+    CreatePlanet();
+    CreateAtmos();
+    CreateDetails();
+
+    generations++;
+}
+
+
+
+// Creates a planet
+function CreatePlanet() {
+    // Terrain and atmosphere colours
+	baseTerrain = color(random(50, 255), random(50, 255), random(50, 255));
+    let colourShift = random(-100, -20);
+    terrainDetail = color(red(baseTerrain) + colourShift, green(baseTerrain) + colourShift, blue(baseTerrain) + colourShift);
+
+    // Create array of vertices for terrain
+	for (let i = 0; i <= res1; i++) {
+        let lon = map(i, 0, res1, -180, 180);
+        terrainColours[i] = []; // Create 2d array + clear array on multiple generations
+		globe[i] = [];
+		for (let j = 0; j <= res1; j++) {
+			let lat = map(j, 0, res1, -90, 90);
+
+			let x = r1 * sin(lon) * cos(lat);
+			let y = r1 * sin(lon) * sin(lat);
+			let z = r1 * cos(lon);
+
+            // These are needed otherwise the noise stays the same until reload rather than changing every new planet
+            let nX = x * generations;
+            let nY = y * generations;
+            let nZ = z * generations;
+
+            globe[i].push(createVector(x + map(noise(nX, nY, nZ), 0, 1, -10, 10), y + map(noise(nY, nX, nZ), 0, 1, -10, 10), z + map(noise(nZ, nX, nY), 0, 1, -10, 10)));
+
+            // Choose between base and detail colour for each vertex
+            let whichColour = round(random());
+            switch (whichColour) {
+                case 1:
+                    terrainColours[i].push(baseTerrain);
+                    break;
+                
+                default:
+                    terrainColours[i].push(terrainDetail);
+                    break;
+            }
+		}
+	}
+}
+
+
+
+// Creates an atmosphere
+function CreateAtmos(){
+    baseAtmos = color(random(100, 255), random(100, 255), random(100, 255), random(75, 125));
+    r2 = random(210, 220); // atmosphere radius
+
+    // Create array of vertices for atmosphere
+    for (let i = 0; i <= res2; i++) {
+        let lon = map(i, 0, res2, -180, 180);
+		atmos[i] = [];
+		for (let j = 0; j <= res2; j++) {
+			let lat = map(j, 0, res2, -90, 90);
+
+			let x = r2 * sin(lon) * cos(lat);
+			let y = r2 * sin(lon) * sin(lat);
+			let z = r2 * cos(lon);
+
+			atmos[i].push(createVector(x, y, z));
+		}
+	}
+}
+
+
+
+// Creates background stars
+function CreateStars() {
+    // Create arrays attributes & positions for stars
+	for (let i = 0; i < numStars; i++) {
+		starsX.push(random(-1920, 1920)); // Using an array of vectors didn't work so using separate arrays for x and y 
+		starsY.push(random(-1080, 1080));
+		starSize.push(random(1, 2));
+		starColours.push(color(random(200, 255), random(200, 255), random(200, 255)));
+	}
+}
+
+
+
+function CreateDetails() {
+    trees = []; // Clear trees list
+
+    // Create array of vertices for trees
+    for (let i = 0; i < numtrees; i++) {
+        let mainList = floor(random(globe.length)); // ---------->
+        let subList = floor(random(globe[mainList].length));// --> because it's a 2d array
+        trees.push(globe[mainList][subList]);
+    }
+}
+
+
+
+// Save a 1440p picture of the canvas
 function Save() {
     resizeCanvas(2560, 1440);
     save("planet.png")
     resizeCanvas(windowWidth, windowHeight);
+}
+
+
+
+// Re-generate planet on enter key pressed
+function keyPressed() {
+    if (keyCode === ENTER) {
+        Generate();
+    }
 }
